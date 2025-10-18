c2752f779ed1fa943d75137c8fbaacb50f25f5f5
jakubstenc <jakubstenc@gmail.com>
2025-10-18 16:55:59 +0200

    build

--- _build.R
+++ _build.R
@@ -7,6 +7,14 @@
 rm(list = ls())
 message("✔ Environment cleared.")
 
+# The path comes from your error message
+corrupt_path <- "~/R/x86_64-pc-linux-gnu-library/4.5/HanPolNet"
+unlink(corrupt_path, recursive = TRUE)
+
+# Double-check it's gone
+file.exists(corrupt_path)
+#> [1] FALSE
+
 # --- 2. Document the Package ---
 # Generate .Rd files and update NAMESPACE from roxygen2 comments.
 devtools::document()
